f80cf93b5f205ab11d932b32efc6c75e77eda96c
nthqu4n <hoangquan26102002@gmail.com>
2021-12-12 22:15:38 +0700

    .

---
+++ finalc/config.py
@@ -0,0 +1,35 @@
+# ------------ Socket ------------ #
+
+HOST          = "127.0.0.1" 
+SERVER_PORT   = 65432
+FORMAT        = "utf8"
+
+# ---------- DB protocol --------- #
+
+FIND_BY_ID    = "find_by_id"
+SAVE_ALL_IMGS = "save_all_imgs"
+FETCH_DATA    = "fetch_data"
+CHECK         = "check"
+DISCONNECT    = "disconnect"
+
+FAIL          = "fail"
+GOOD          = "good"
+SUCCESS       = "success"
+
+# ------------ DB info ----------- #
+
+TABLE_NAME    = "Phonebook"
+ID            = "ID"
+AVATAR        = "Avatar"
+FULLNAME      = "FullName"
+PHONENUMBER   = "Phonenumber"
+EMAIL         = "Email"
+
+# ---- SQL, Login credentials ---- #
+
+SERVER        = "socket-skrt.database.windows.net"
+DATABASE      = "SocketProject"
+UID           = "nthquan"
+PWD           = "socket123#"
+
+# --------- Count client --------- #
